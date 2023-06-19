4b75b238b6ae4571278f9becea1042dde3a2ed84
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-06-18 23:45:58 -0400

    create GET api to fetch prompts from the Feed, create the PromptCardList component that accepts the data from the useEffect to pass the the PromptCard component to be built next

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,50 @@
-import React from "react";
-
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [post, setPost] = useState([]);
+
+  const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+      <div className="mt-16 prompt_layout">
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        ))}
+      </div>
+    );
+  };
+
+  const handleSearchChange = (e) => {};
+
+  //fetch post form created GET api route
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/prompt");
+      const data = await res.json();
+      setPost(data);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          value={searchText}
+          onChange={handleSearchChange}
+          placeholder="Search for a tag or a username"
+          required
+          className="search_input"
+        />
+      </form>
+      <PromptCardList data={post} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
