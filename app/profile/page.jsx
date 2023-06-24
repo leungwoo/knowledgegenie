9b96d855c1ffa6f401780f55039629c805d59032
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-06-24 15:37:51 -0400

    create updateprompt route, update api

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,11 +1,47 @@
-import Profile from "@components/Profile";
+"use client";
 
-const page = () => {
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+//import Loading from "@components/Loading";
+import { useRouter } from "next/Navigation";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPost(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async () => {};
+
   return (
     <div>
-      <Profile />
+      <Profile
+        name={"My"}
+        desc={
+          "Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
+        }
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+        data={post}
+      />
     </div>
   );
 };
 
-export default page;
+export default MyProfile;
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,24 +1,33 @@
 "use client";
 
+import { useSession } from "next-auth/react";
 import Image from "next/image";
-import { useRouter } from "next/Navigation";
+import { usePathname, useRouter } from "next/Navigation";
 import { useState } from "react";
 
-const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-  //check if copied already
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
   const router = useRouter();
+
   const [copied, setCopied] = useState("");
+
+  const handleProfileClick = () => {
+    console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => setCopied(""), 3000);
+    setTimeout(() => setCopied(false), 3000);
   };
 
-  const handleProfileClick = () => {
-    router.push("/profile");
-  };
   return (
-    <div className="flex-1 break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-6 pb-4 backdrop-blur-lg backdrop-filter md:w-[360px] w-full h-fit">
+    <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div
           className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
@@ -31,15 +40,17 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             height={40}
             className="rounded-full object-contain"
           />
+
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
               {post.creator.username}
             </h3>
-            <p className="text-inter text-sm font-gray-500">
+            <p className="font-inter text-sm text-gray-500">
               {post.creator.email}
             </p>
           </div>
         </div>
+
         <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
@@ -47,19 +58,37 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                 ? "/assets/icons/tick.svg"
                 : "/assets/icons/copy.svg"
             }
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
             width={12}
             height={12}
-            alt="copy"
           />
         </div>
       </div>
-      <p className="my-4 font-satoshi text-md text-gray-700">{post.prompt}</p>
+
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
