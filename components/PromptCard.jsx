e2e13e548a39f912f09cab7d273171569b0e23a6
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-07-02 13:47:12 -0400

    fix lines in card

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -63,7 +63,9 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
         </div>
       </div>
 
-      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p className="my-4 font-satoshi text-sm text-gray-700 line-clamp-4">
+        {post.prompt}
+      </p>
       <p
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
